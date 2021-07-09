9dc45d41928c58d97a7a9b944cd8b6f8a18c328e
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2021-07-09 13:56:56 +0200

    Working prototype

--- compile.sh
+++ compile.sh
@@ -4,8 +4,4 @@ TASK=ExtremaFinding
 TASK1=GlobalMinimumFinder
 TASK2=LocalMinimaFinder
 
-g++ -c -I ./src/numerical_recipes ./src/numerical_recipes/*.cpp
-g++ -c -I ./src/ExtremaFinding ./src/ExtremaFinding/*.cpp
-g++ -std=c++11 -Wall -O2 -static -o grader.cpp -I .
-#rm *.o
-#g++ -std=c++11 -Wall -O2 -static -o bin/${TASK} grader.cpp -Ibin/numerical_recipes.o -I${TASK} # ${TASK}/${TASK1}.cpp ${TASK}/${TASK2}.cpp 
+g++ -std=c++11 -Wall -O2 -static -o bin/${TASK} grader.cpp -Isrc/numerical_recipes src/numerical_recipes/*.cpp -Isrc/${TASK} src/${TASK}/*.cpp
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ TASK=ExtremaFinding
 TASK1=GlobalMinimumFinder
 TASK2=LocalMinimaFinder
 
-g++ -std=c++11 -Wall -O2 -static -o bin/${TASK} grader.cpp src/${TASK}/${TASK1}.cpp #${TASK}/${TASK2}.cpp
+g++ -std=c++11 -Wall -O2 -static -o bin/${TASK} grader.cpp -Isrc/numerical_recipes src/numerical_recipes/*.cpp -Isrc/${TASK} src/${TASK}/*.cpp
+
 ./bin/${TASK}
