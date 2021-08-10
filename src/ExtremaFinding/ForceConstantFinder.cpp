21b83c447309fa14d2f306bb5531b0317d65d9ba
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2021-08-10 11:45:49 +0200

    Fix, finalize exact method to find force constant

--- src/ExtremaFinding/ForceConstantFinder.cpp
+++ src/ExtremaFinding/ForceConstantFinder.cpp
@@ -31,25 +31,54 @@ vector<double> find_with_simplicity_accuracy_trading(
     // initialization
     int multiplicities[6] = {0, 1, 2, 3, 4, 6};
     double simplicity_accuracy_trading[6] = {0.05, 0.1, 0.15, 0.2, 0.25, 0.35};
-    vector<double> force_constants;
+    vector<double> force_constants, threshold;
+    vector<pair<double, int> > auxiliary;
     force_constants.resize(number_of_terms);
+    threshold.resize(number_of_terms);
+    auxiliary.resize(number_of_terms);
+
+    double sum_of_squares_of_energy = 0;
+    for (int i = 0; i < (int) angles.size(); i++)
+    {
+        sum_of_squares_of_energy += quantum_mechanics_points[i] * quantum_mechanics_points[i];
+    }
 
     // calculate force constants directly as Cosine Transform coefficients
     for (int i = 0; i < number_of_terms; i++)
     {
         force_constants[i] = optimal_force_constant(angles, quantum_mechanics_points, multiplicities[i]);
-        // it is inoptimal to optimize for this harmonic
-        /*
-        if(simplicity_accuracy_trading[i] - // tolerance
-            angles.size() / 2.0 * force_constants[i] * force_constants[i] > 0)
+        double error_contributor = (i ? angles.size() / 2.0 : angles.size()) * force_constants[i] * force_constants[i];
+        double scaled_sat = simplicity_accuracy_trading[i] * sqrt(angles.size());
+        if (error_contributor < scaled_sat * scaled_sat)
         {
-            force_constants[i] = 0;
+            threshold[i] = 0;
         }
-        if(force_constants[i] < cutoff)
+        else
         {
-            force_constants[i] = 0;
+            threshold[i] = error_contributor + 
+                        (error_contributor - 
+                            scaled_sat * 
+                            scaled_sat)/
+                        (2.0 * scaled_sat) * 
+                        (error_contributor - 
+                            scaled_sat * 
+                            scaled_sat)/
+                        (2.0 * scaled_sat);
         }
-        //*/
+        auxiliary[i] = {threshold[i], i};
+    }
+    sort(auxiliary.begin(), auxiliary.end(), greater<pair<double, int> >());
+    for (int i = 0; i < number_of_terms; i++)
+    {
+        int idx = auxiliary[i].second;
+        if (sum_of_squares_of_energy <= auxiliary[i].first)
+        {
+            sum_of_squares_of_energy -= (idx ? angles.size() / 2.0 : angles.size()) * force_constants[idx] * force_constants[idx];
+        }
+        else
+        {
+            force_constants[idx] = 0;
+        }
     }
 
     return force_constants;
