562ccebaf547d07c030403a76c3447f3bace7377
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2021-09-24 11:45:36 +0200

    Add comment to GlobalMinimumFinder.h

--- src/ExtremaFinding/GlobalMinimumFinder.h
+++ src/ExtremaFinding/GlobalMinimumFinder.h
@@ -3,6 +3,14 @@ using namespace std;
 
 static vector<double> dummy;
 
+/* 
+Simulated annealing optimization.
+objective_function: function to be optimized
+D: the dimension of the input
+r: the initial radius
+number_of_steps: number of steps, default to be
+1e6.
+ */
 vector<double> simulated_annealing(
     function<double(vector<double>&)> objective_function, 
     int D, 
